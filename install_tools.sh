4268e8e2599b05146f65b21ceb6fec05973559a9
EJLP <ejlp12@gmail.com>
2021-04-27 07:43:32 +0700

    Create install_tools.sh

---
+++ install_tools.sh
@@ -0,0 +1,27 @@
+sudo curl --silent --location -o /usr/local/bin/kubectl \
+   https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.11/2020-09-18/bin/linux/amd64/kubectl
+sudo chmod +x /usr/local/bin/kubectl
+
+#update aws cli
+sudo pip install --upgrade awscli && hash -r
+
+#install other tools
+sudo yum -y install jq gettext bash-completion moreutils
+
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+for command in kubectl jq envsubst aws
+  do
+    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+  done
+
+kubectl completion bash >>  ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+echo 'export LBC_VERSION="v2.0.0"' >>  ~/.bash_profile
+.  ~/.bash_profile
+
+
